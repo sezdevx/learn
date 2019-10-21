db33108778e368d5ce8f5d60f7720bb6eff7958e
Sezgin <>
2019-10-21 18:44:54 +0100

    comment

--- bash/comment_counter.sh
+++ bash/comment_counter.sh
@@ -1,15 +1,31 @@
 #!/usr/bin/env bash
 
+file=""
+if [[ $# != 0 ]]; then
+    file="$*"
+fi
+
 counter=0
 line=0
 # counts the number of lines that start with #
-while IFS= read -r var ; do
-    if [[ $var =~ ^[[:space:]]*#.* ]]; then
-        if [[ $line != 0 ]]; then
-            (( counter++ ))
-        fi
-    fi
-    (( line++ ))
-done
 
+if [[ -n $file ]]; then
+    while IFS= read -r var ; do
+        if [[ $var =~ ^[[:space:]]*#.* ]]; then
+            if [[ $line != 0 ]]; then
+                (( counter++ ))
+            fi
+        fi
+        (( line++ ))
+    done < "$file"
+else
+    while IFS= read -r var ; do
+        if [[ $var =~ ^[[:space:]]*#.* ]]; then
+            if [[ $line != 0 ]]; then
+                (( counter++ ))
+            fi
+        fi
+        (( line++ ))
+    done
+fi
 echo "Number of comment lines: " $counter
